5a6102e84e83ed02fe714477f6080278a7ec06c8
tpldrdm <dioanarosdemesa@tpl.com.sg>
2020-10-25 01:07:53 +0800

    behat - homepage test success

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,7 +7,8 @@ use Behat\Gherkin\Node\TableNode;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+// class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements Context, SnippetAcceptingContext
+class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements Context
 {
     /**
      * Initializes context.
