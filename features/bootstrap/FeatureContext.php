661b37fab4780d11be94b66e2e8c821bc6313854
tpldrdm <dioanarosdemesa@tpl.com.sg>
2020-10-25 08:54:11 +0800

    behat - test calc feature

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -19,5 +20,21 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements 
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given the method :arg1 receives the numbers :arg2 and :arg3
+     */
+    public function theMethodReceivesTheNumbersAnd($arg1, $arg2, $arg3)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the calculated value should be :arg1
+     */
+    public function theCalculatedValueShouldBe($arg1)
+    {
+        throw new PendingException();
     }
 }
