53af8f971b14bbcc8497a5fb09859e4ade3eb791
tpldrdm <dioanarosdemesa@tpl.com.sg>
2020-10-25 18:47:50 +0800

    Loan model model and auth scaffolding

---
+++ database/migrations/2020_10_25_104148_create_loans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->bigIncrements('loan_id');
+            $table->float('amount_reqd');
+            $table->integer('term_month');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
