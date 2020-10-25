6ef993d0403d1e2d12cdb04d5a7d469f68ea7f51
tpldrdm <dioanarosdemesa@tpl.com.sg>
2020-10-25 11:35:47 +0800

    use collaborators and matchers

--- spec/TodoListSpec.php
+++ spec/TodoListSpec.php
@@ -4,6 +4,8 @@ namespace spec\Predmond\Calculator;
 
 use PhpSpec\ObjectBehavior;
 use Predmond\Calculator\TodoList;
+use Predmond\Calculator\TaskCollection;
+use Predmond\Calculator\Task;
 
 class TodoListSpec extends ObjectBehavior
 {
@@ -11,4 +13,23 @@ class TodoListSpec extends ObjectBehavior
     {
         $this->shouldHaveType(TodoList::class);
     }
+    function it_adds_a_task_to_the_list(TaskCollection $tasks, Task $task)
+    {
+        $tasks->add($task)->shouldBeCalledTimes(1);
+        $this->tasks = $tasks;
+ 
+        $this->addTask($task);
+    }
+    function it_checks_whether_it_has_any_tasks(TaskCollection $tasks)
+	{
+	    $tasks->count()->willReturn(0);
+	    $this->tasks = $tasks;
+	 
+	    $this->hasTasks()->shouldReturn(false);
+	 
+	    $tasks->count()->willReturn(20);
+	    $this->tasks = $tasks;
+	 
+	    $this->hasTasks()->shouldReturn(true);
+	}
 }
--- src/TodoList.php
+++ src/TodoList.php
@@ -2,6 +2,22 @@
 
 namespace Predmond\Calculator;
 
+use Predmond\Calculator\Task;
+
 class TodoList
 {
+	public $tasks;
+
+    public function addTask(Task $task)
+    {
+        $this->tasks->add($task);
+    }
+
+    public function hasTasks()
+    {
+        if ($this->tasks->count() > 0)
+        return true;
+ 
+    	return false;
+    }
 }
